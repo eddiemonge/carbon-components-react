7d2c96188f2dbc23cff58febc212275c10a7f159
Eddie Monge <eddie@us.ibm.com>
2017-10-09 11:34:04 -0700

    chore(React): update to React@16 and related
    - Update React and its related libraries to 16
    - Update Enzyme and Jest to work with React@16
    - Update tests with updated/changed methods

--- setup-jest.js
+++ setup-jest.js
@@ -1,1 +1,8 @@
-window.requestAnimationFrame = (callback) => { callback(); };
+global.requestAnimationFrame = function(callback) {
+  setTimeout(callback, 0);
+};
+
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
